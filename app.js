0d07c9526995dc672c0ddd587c16edab6b75da07
GonçaloDiogoCoelho <goncalodmvm.c@gmail.com>
2022-11-02 12:05:47 +0000

    first commit

--- app.js
+++ app.js
@@ -5,16 +5,25 @@ const app = express();
 const port = 5000;
 
 const welcome = (req, res) => {
-  res.send("Welcome to my favourite movie list");
+  res.send("Welcome to my favorite users and movie list");
 };
 
 app.get("/", welcome);
 
+
+//* MOVIES ====================================
 const movieHandlers = require("./movieHandlers");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 
+//* USERS ====================================
+const usersHandler = require("./usersHandler")
+
+app.get("/api/users", usersHandler.getUsers);
+app.get("/api/users/:id", usersHandler.getUsersById);
+
+//* LISTEN ====================================
 app.listen(port, (err) => {
   if (err) {
     console.error("Something bad happened");
